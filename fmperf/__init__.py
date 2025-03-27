ece4dc9411397af3535274f4b6f0a41c4a429d53
Chen Wang <Chen.Wang1@ibm.com>
2025-03-27 18:25:28 +0000

    Add fmperf library and update dependencies
    Signed-off-by: Chen Wang <Chen.Wang1@ibm.com>

---
+++ fmperf/__init__.py
@@ -0,0 +1,18 @@
+from fmperf.ModelSpecs import TGISModelSpec, vLLMModelSpec
+from fmperf.Cluster import Cluster, WorkloadSpec, DeployedModel
+from fmperf.WorkloadSpecs import (
+    HeterogeneousWorkloadSpec,
+    HomogeneousWorkloadSpec,
+    RealisticWorkloadSpec,
+)
+
+__all__ = [
+    "TGISModelSpec",
+    "vLLMModelSpec",
+    "Cluster",
+    "WorkloadSpec",
+    "DeployedModel",
+    "HeterogeneousWorkloadSpec",
+    "HomogeneousWorkloadSpec",
+    "RealisticWorkloadSpec",
+]
---
+++ fmperf/utils/__init__.py
@@ -0,0 +1,15 @@
+from fmperf.utils.Waiting import Waiting
+from fmperf.utils.Creating import Creating
+from fmperf.utils.Deleting import Deleting
+from fmperf.utils.Logging import make_logger
+from fmperf.utils.Parsing import parse_results
+from fmperf.utils.Benchmarking import run_benchmark
+
+__all__ = [
+    "Waiting",
+    "Creating",
+    "Deleting",
+    "make_logger",
+    "parse_results",
+    "run_benchmark",
+]
